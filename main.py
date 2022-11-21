e659488ef1814080eb86a0296545d8ced6da8ec3
Wi <nurindahpratiwi71@gmail.com>
2022-11-21 22:54:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+""" Module ini berfungsi untuk memanggil menu aplikasi super cashier
+aplikasi super cashier dapat dijalankan jika database "cashier" dan table "transaction" sudah didefinisikan
+"""
+
 from transaction_file import menu
 
-if __name__ == "__main__":
+if __name__ == "__main__": # memanggil menu aplikasi super cashier
     menu()
